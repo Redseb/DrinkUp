37914c05b0c79ce2464886ffb3ac902cdb2986ca
Redseb <dwarfslikepie@gmail.com>
2020-09-20 14:28:03 +0200

    Created card component (no gesture handler yet) and back button in GameScreen

---
+++ src/components/Card.js
@@ -0,0 +1,83 @@
+import React, {useEffect} from 'react';
+import {StyleSheet, View, Text, Dimensions} from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  Easing,
+  withTiming,
+  repeat,
+} from 'react-native-reanimated';
+
+const {width, height} = Dimensions.get('screen');
+
+const Card = ({title, desc, type}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>{title}</Text>
+      </View>
+
+      <Text style={styles.desc}>{desc}</Text>
+      <View style={styles.logoContainer}>
+        <Text
+          style={[
+            styles.logo,
+            {marginBottom: height / 150, fontSize: height / 20},
+          ]}>
+          🍻
+        </Text>
+        <Text style={styles.logo}>Drink Up! </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignSelf: 'center',
+    height: height / 1.3,
+    width: width / 1.15,
+    backgroundColor: '#303030',
+    borderRadius: 10,
+    borderColor: '#FDD451',
+    borderWidth: 2,
+    elevation: 10,
+  },
+  titleContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#FDD451',
+    borderRadius: 10,
+    marginHorizontal: width / 20,
+    marginTop: height / 40,
+    padding: height / 150,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: height / 20,
+    fontFamily: 'FjallaOne',
+    color: '#303030',
+  },
+  desc: {
+    textAlign: 'center',
+    fontSize: height / 20,
+    fontFamily: 'FjallaOne',
+    color: '#FDD451',
+  },
+  logoContainer: {
+    marginBottom: height / 40,
+  },
+  logo: {
+    fontSize: height / 40,
+    fontFamily: 'FjallaOne',
+    color: '#FDD451',
+    textAlign: 'center',
+  },
+});
+
+export default Card;
--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -1,7 +1,18 @@
 import React, {useEffect, useRef} from 'react';
-import {StyleSheet, View, Text, Dimensions, BackHandler} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  BackHandler,
+  TouchableOpacity,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const {width, height} = Dimensions.get('screen');
+
+//Components
+import Card from '../components/Card';
 
 const GameScreen = ({players, setPlayers, setInGame}) => {
   //Set up BackHandler to go back to title screen when pressing back button (ANDROID)
@@ -20,18 +31,43 @@ const GameScreen = ({players, setPlayers, setInGame}) => {
 
   return (
     <View style={[styles.container, StyleSheet.absoluteFillObject]}>
-      <Text>GameScreen</Text>
-      <Text>{players[0].name}</Text>
+      <View style={[styles.backButtonContainer, StyleSheet.absoluteFillObject]}>
+        <TouchableOpacity
+          onPress={() => {
+            setInGame(false);
+          }}>
+          <Icon name="arrow-back" size={height / 18} color="#FDD451" />
+        </TouchableOpacity>
+      </View>
+      <Card
+        title={'👩 Longest Hair 👩'}
+        desc={'The player with the longest hair drinks'}
+        type={'Default'}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    height: height,
+    width: width,
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignContent: 'center',
+  },
+  backButtonContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#303030',
+    width: height / 18,
+    height: height / 18,
+    marginTop: height / 20,
+    marginLeft: width / 30,
+    borderRadius: 10,
+    elevation: 5,
   },
 });
 
